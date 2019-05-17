ed9c09f18bf87db2e91c520597e2815b271a6f56
Ayush616 <47354211+Ayush616@users.noreply.github.com>
2019-05-17 21:19:04 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,11 +1,9 @@
 <?php
-
-$connection= mysqli_connect('shareddb-j.hosting.stackcp.net', 'diary616-3935a18a', 'ayush616', 'diary616-3935a18a');
-
-if(mysqli_connect_error()){
-    die("Database Connection Failed..");
-}
-
-
+$connection = mysqli_connect('shareddb-j.hosting.stackcp.net', 'diary616-3935a18a', 'ayush616', 'diary616-3935a18a');
+    
+if (mysqli_connect_error()){
+    
+    die("Database connection error!!");
+}    
 
 ?>
